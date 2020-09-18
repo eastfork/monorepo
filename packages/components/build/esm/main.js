d0fd1706aa324149a2838838eec5d07685b097a0
Ben Lewis <johnbenjaminlewis@gmail.com>
2020-09-18 18:11:56 -0400

    uncommit compiled files

--- packages/components/build/esm/main.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-export const Main = ({ text }) => React.createElement("main", null, text);
-export const Card = ({ heading, text }) => (React.createElement("div", { className: "card" },
-    React.createElement("div", { className: "card-heading" }, heading),
-    React.createElement("div", { className: "card-body" }, text)));
-//# sourceMappingURL=main.js.map
