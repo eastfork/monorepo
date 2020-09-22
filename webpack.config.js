017b34707de853001b3a9a7b41d9083de0513635
Ben Lewis <johnbenjaminlewis@gmail.com>
2020-09-22 19:41:31 -0400

    Adding default build targets to tsconfig.jsons

--- webpack.config.js
+++ webpack.config.js
@@ -11,7 +11,7 @@ module.exports = {
     rules: [
       {
         test: /\.tsx?$/,
-        loader: '@ts-tools/webpack-loader',
+        loader: 'ts-loader',
       },
       {
         test: /\.css$/,
